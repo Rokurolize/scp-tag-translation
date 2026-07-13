ae938da6929d724bb45afca13ca0cc76d1f3866c
Rokurolize <1701388+Rokurolize@users.noreply.github.com>
2026-07-13 15:02:46 +0900

    desloppify: remove unused tag loader

--- scripts/domain/tag_policy.py
+++ scripts/domain/tag_policy.py
@@ -18,7 +18,6 @@ from scripts.domain.tag_models import (
     SpecialTranslationAction,
     SourceTagPolicy,
 )
-from scripts.domain.tag_validation import validate_tag_records
 
 ROOT = Path(__file__).resolve().parents[2]
 DATA_EN = ROOT / "data" / "en_tags.json"
@@ -486,11 +485,3 @@ def build_jp_policy(inputs: JpPolicyInputs) -> JpPolicyDocument:
             )
         },
     }
-
-
-def load_tag_records() -> tuple[list[EnTag], list[JpTag], list[DeprecatedTag]]:
-    return validate_tag_records(
-        load_json(DATA_EN),
-        load_json(DATA_JP),
-        load_json(DATA_DEPRECATED),
-    )
